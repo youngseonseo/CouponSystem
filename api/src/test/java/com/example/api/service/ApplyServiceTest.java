6530184a7697abfd151555d719499b3868b9bd36
youngseonseo <sue000731@naver.com>
2024-05-02 22:00:28 +0900

    feat: 쿠폰 발급 로직

---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,55 @@
+package com.example.api.service;
+
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번응모(){
+        applyService.apply(1L);
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러번응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i =0; i <threadCount; i++){
+            long userId = i;
+            executorService.submit(() -> {
+                try{
+                    applyService.apply(userId);
+                } finally{
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
+    }
+}
